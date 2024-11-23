87c60cd7d172c0410d3378d35abb323b1e3642b7
RFS <7137874+rfs85@users.noreply.github.com>
2024-11-23 21:52:03 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,18 +2,10 @@
 
 
 
-
-```bash
-root@devsecops:/etc/apt# apt install sudo git docker.io docker-compose
-```
-
-```bash
-rfs@devsecops:~/DevSecOps_Lab$ sudo docker network create redlabs-network
-```
-
-```bash
-rfs@devsecops:~/DevSecOps_Lab$ sudo docker-compose up -d
-```
+apt update -y
+apt install sudo git docker.io docker-compose
+docker network create redlabs-network
+docker-compose up -d
 
 
 
