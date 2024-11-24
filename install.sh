613affffab92754a63fe1bc6e37b2b7900c8979a
RFS <7137874+rfs85@users.noreply.github.com>
2024-11-24 14:10:23 +0000

    dfdfg

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 
 
 apt update -y
-apt install sudo git docker.io docker-compose
+apt install sudo git docker.io docker-compose apt-transport-https ca-certificates curl gnupg-agent software-properties-common
 docker network create redlabs-network
 docker-compose up -d
 
@@ -11,4 +11,14 @@ docker-compose up -d
 
 
 
+destroy(){
+    sudo docker ps --format "table {{.Names}}\t{{.Ports}}\t{{.Status}}"
+    # Stop All Conteinrs
+    sudo docker stop $(sudo docker ps -a -q)
 
+    # Remove all containers
+    sudo docker rm $(sudo docker ps -a -q)
+    
+    # Remove all container images
+    sudo docker rmi $(sudo docker images -q) 
+}
